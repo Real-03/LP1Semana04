37d33022c64ab3d448dcf9b8c9e849fdf91304a4
Tomás Real <tomasreal.art3d@gmail.com>
2025-03-13 13:33:35 +0000

    Add BetterDecor

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor(args[0],Convert.ToChar(args[1]),Convert.ToInt32(args[2])));
+            
+        }
+        private static string Decor(string s, char dec, int num)
+        {
+            string total = "";
+            for(int i =0; i<num; i++)
+            {
+                total+=dec;
+            }
+            return $"{total} {s} {total}";
+        }
+    }
+}
