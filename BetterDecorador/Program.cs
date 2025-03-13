125dca9d0791909f8d23226688ccee99e7c0aba2
Tomás Real <tomasreal.art3d@gmail.com>
2025-03-13 13:36:53 +0000

    Add comments XML

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,11 +4,22 @@ namespace BetterDecorador
 {
     public class Program
     {
+        /// <summary>
+        /// Chama um metodo que decora uma string
+        /// </summary>
+        /// <param name="args">array de strings</param>
         private static void Main(string[] args)
         {
             Console.WriteLine(Decor(args[0],Convert.ToChar(args[1]),Convert.ToInt32(args[2])));
             
         }
+        /// <summary>
+        /// Decora a string
+        /// </summary>
+        /// <param name="s">String a decorar</param>
+        /// <param name="dec">Simbolo para decorar</param>
+        /// <param name="num">Numero de vezes que o simbolo aparece</param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int num)
         {
             string total = "";
